c8267020501607a897c187710644ae968e5c86be
Alex2009-creator <alexruss2009@gmail.com>
2026-04-29 21:17:53 +0300

    Очистка дефолтныйх стилей

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,27 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  </header>
+  <div class="box">
+    <header class="box-header">
+      <div class="box-header__logo">
+        <img src="./assets/logo.png" />
+      </div>
+      <div class="box-header__nav"></div>
+    </header>
+    <main class="box-main">
+      <div class="box-main__sidebar"></div>
+      <div class="box-main__container"></div>
+    </main>
+  </div>
 </template>
 
 <style scoped>
+.box {
+  min-height: 100vh;
+  background-color: var(--background-bg);
+}
+
 header {
   line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
