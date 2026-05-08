61889d5b3802e23579905a262a8ea90c4213fc43
Alex2009-creator <alexruss2009@gmail.com>
2026-05-08 18:02:01 +0300

    Стилизация компонентов

---
+++ src/components/ButtonStart.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts"></script>
+
+<template>
+    <button
+        type="button"
+        class="box-main-card-container__btn"
+    >
+        <span class="box-main-card-container__text">Начать</span>
+        <img
+            src="../assets/btn-start.png"
+            alt="Начать"
+            class="box-main-card-container__img"
+        />                    
+    </button>
+</template>
+
+<style scoped>
+    .box-main-card-container__btn {
+        min-width: 110px;
+        min-height: 40px;
+        border-radius: 16px;
+        padding: 0;
+        background-color: var(--background-bg);
+        color: var(--background-card);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .box-main-card-container__btn:hover {
+        cursor: pointer;
+    }
+
+    .box-main-card-container__text {
+        font-weight: 500;
+        font-size: 15px;
+        margin: 0 5px;
+    }
+
+    .box-main-card-container__img {
+        width: 13px;
+        height: 13px;
+        margin: 0 5px;
+    }
+</style>
--- src/components/RelaxCard.vue
+++ src/components/RelaxCard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
     import type { CardProps } from '@/interfaces/cardprops.interface'
+import ButtonStart from './ButtonStart.vue';
 
     const {
         title,
@@ -11,8 +12,11 @@
 <template>
     <div class="box-main-card">
         <h1 class="box-main-card__title">{{ title }}</h1>
-        <p>{{ description }}</p>
-        <p> {{ duration }} мин.</p>
+        <p class="box-main-card__text">{{ description }}</p>
+        <div class="box-main-card-container">
+            <ButtonStart />
+            <p class="box-main-card__label">{{ duration }} мин</p>
+        </div>
     </div>
 </template>
 
@@ -22,6 +26,31 @@
         height: 170px;
         border-radius: 20px;
         background-color: var(--background-card);
-        padding: 20px;
+        padding: 20px 30px;
+    }
+
+    .box-main-card-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    .box-main-card__title {
+        font-weight: 500;
+        font-size: 25px;
+        color: var(--color-text);
+    }
+
+    .box-main-card__text {
+        font-weight: 500;
+        font-size: 15px;
+        color: var(--color-text);
+    }
+
+    .box-main-card__label {
+        font-weight: 500;
+        font-size: 15px;
+        color: var(--color-text-light);
+        margin-right: 13px;
     }
 </style>
